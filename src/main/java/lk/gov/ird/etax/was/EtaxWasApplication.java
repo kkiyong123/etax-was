59727987e962d762a9982ce2ff281166cf2646e2
kkiyong123 <98229313+kkiyong123@users.noreply.github.com>
2026-07-14 22:25:24 +0900

    Add src/main/java/lk/gov/ird/etax/was/EtaxWasApplication.java

---
+++ src/main/java/lk/gov/ird/etax/was/EtaxWasApplication.java
@@ -0,0 +1,11 @@
+package lk.gov.ird.etax.was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaxWasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EtaxWasApplication.class, args);
+    }
+}
